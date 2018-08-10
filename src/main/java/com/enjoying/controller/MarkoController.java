d9f52204ff28b0906e60afe6ba129f5b120a25ef
Gavrilo <gavrilo.adamovic@pmf.edu.rs>
2018-08-10 14:30:09 +0200

    Added JPA for h2 database

--- src/main/java/com/enjoying/controller/MarkoController.java
+++ src/main/java/com/enjoying/controller/MarkoController.java
@@ -1,15 +1,27 @@
 package com.enjoying.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.enjoying.jpa.StudentRepository;
+import com.enjoying.model.Student;
+
 @Controller
 public class MarkoController {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
 	@RequestMapping(value="/helloMarko", method=RequestMethod.GET)
 	public String sayHelloMarko(ModelMap model)
 	{	
+		
+		studentRepository.save(new Student("Pera","12345"));
+		System.out.println(studentRepository.findAll());
+		
 		model.addAttribute("message","Hello from Marko!");
 		return "helloMarko";
 	}
--- src/main/java/com/enjoying/enjoyingTest/EnjoyingMainApplication.java
+++ src/main/java/com/enjoying/enjoyingTest/EnjoyingMainApplication.java
@@ -2,10 +2,14 @@ package com.enjoying.enjoyingTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.enjoying.controller")
+@ComponentScan(basePackages = "com.enjoying")
+@EnableJpaRepositories("com.enjoying.jpa")
+@EntityScan("com.enjoying.model")
 public class EnjoyingMainApplication {
 
 	public static void main(String[] args) {
