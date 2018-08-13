41e2bad43992037fb5555eece5d44a39ce76ed08
Gavrilo <gavrilo.adamovic@pmf.edu.rs>
2018-08-13 15:03:06 +0200

    Added controller for logging in students

--- src/main/java/com/enjoying/controller/MarkoController.java
+++ src/main/java/com/enjoying/controller/MarkoController.java
@@ -1,10 +1,14 @@
 package com.enjoying.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.enjoying.jpa.RadnikRepository;
 import com.enjoying.jpa.StudentRepository;
@@ -13,26 +17,42 @@ import com.enjoying.model.radnik;
 
 @Controller
 public class MarkoController {
-	
+
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	@Autowired
 	RadnikRepository radnikRepository;
+
+	@RequestMapping(value = "/helloMarko", method = RequestMethod.GET)
+	public String sayHelloMarko(ModelMap model) {
+
+		// studentRepository.save(new Student("Pera","12345"));
+		// System.out.println(studentRepository.findAll());
+		
+		int i = 0;
+		
+		while (i < 100) {
+			radnik r = new radnik("pera");
+			r.setStudent(new Student("Laza", "555555"));
+			radnikRepository.save(r);
+		}
+		
+		System.out.println("i = " + i + " " + radnikRepository.findAll());
+
+		model.addAttribute("message", radnikRepository.findAll());
+		return "helloMarko";
+	}
 	
-	@RequestMapping(value="/helloMarko", method=RequestMethod.GET)
-	 public String sayHelloMarko(ModelMap model)
-	 { 
-	  
-	  //studentRepository.save(new Student("Pera","12345"));
-	  //System.out.println(studentRepository.findAll());
-	  radnik r = new radnik("pera");
-	  r.setStudent(new Student("Laza","555555"));
-	  
-	  radnikRepository.save(r);
-	  System.out.println(radnikRepository.findAll());
-	  
-	  model.addAttribute("message","Hello from Marko!");
-	  return "helloMarko";
-	 }
+	@RequestMapping(value="/logIn", method = RequestMethod.POST)
+	public String logIn(@RequestParam(name = "username") String username, Model model) {
+		List<Student> student = studentRepository.findStudent(username);
+		if(student.size() > 0) {
+			model.addAttribute("attrib","Welcome");
+		}else model.addAttribute("attrib", "Wrong username and password");
+		return "test1";
+	}
+	
+	
+	
 }
--- src/main/java/com/enjoying/jpa/StudentRepository.java
+++ src/main/java/com/enjoying/jpa/StudentRepository.java
@@ -1,6 +1,10 @@
 package com.enjoying.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.enjoying.model.Student;
@@ -8,4 +12,6 @@ import com.enjoying.model.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
+	@Query("from Student st where st.name = :username")
+	public List<Student> findStudent(@Param("username") String username);
 }
